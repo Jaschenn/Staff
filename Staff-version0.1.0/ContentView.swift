abd0c85af8edcd231635ebca1cf6d27fcbd442aa
jaschen <cchenzyy@icloud.com>
2020-02-23 10:49:52 +0800

    Initial Commit

---
+++ Staff-version0.1.0/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Staff-version0.1.0
+//
+//  Created by jas chen on 2020/2/23.
+//  Copyright © 2020 jas chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
